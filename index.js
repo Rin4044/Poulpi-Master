27c253c08baa691c661d526d8875586222bdaad3
Rin4044 <wagnerkealan@yahoo.fr>
2026-02-18 20:48:39 +0100

    Fix Index

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ client.once('ready', async () => {
     const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
     await rest.put(
-        Routes.applicationCommands(client.user.id),
-        { body: commands }
-    );
+    Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID),
+    { body: commands }
+);
 
     console.log("Slash commands mises à jour.");
 
