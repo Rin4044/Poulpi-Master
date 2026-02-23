0f190e09cf1cf5a8cb0f814a7c3d384977d88319
Rin4044 <wagnerkealan@yahoo.fr>
2026-02-23 10:41:48 +0100

    Fix Scheduler

--- scheduler.js
+++ scheduler.js
@@ -3,7 +3,7 @@ const { getNoLiveState, setNoLiveState } = require('./twitchPanel');
 
 function setupScheduler(client) {
 
-    cron.schedule('0 17 * * *', async () => {
+    cron.schedule('0 17 * * 3', async () => {
         if (getNoLiveState()) return;
 
         const channel = await client.channels.fetch(process.env.ANNOUNCE_CHANNEL_ID);
@@ -11,10 +11,14 @@ function setupScheduler(client) {
         await channel.send(
             `@Live Twitch\n\n🇬🇧 I will be live today at 9pm CET!\n🇫🇷 Je serai en live aujourd’hui à 21h !`
         );
+    }, {
+        timezone: 'Europe/Paris'
     });
 
     cron.schedule('0 17 * * 3', () => {
         setNoLiveState(false);
+    }, {
+        timezone: 'Europe/Paris'
     });
 }
 
